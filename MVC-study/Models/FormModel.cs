d1bd15a45dedc30238d8df76b4b0580042df7164
Patryk Lekki <patryk.lekki@awesome-ind.com>
2021-10-19 20:37:44 +0200

    Add project files.

---
+++ MVC-study/Models/FormModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace MVC_study.Models
+{
+    public class FormModel
+    {
+        [Display(Name = "Twoje imię")]
+        public string Name { get; set; }
+        [Display(Name = "Data urodzenia")]
+        public string Date { get; set; }
+    }
+}
